335a5accc4881e939c4fa273065600f1973cb5ee
JumpLao <nattaphat.la@mail.kmutt.ac.th>
2019-01-10 13:36:35 +0700

    Update celery_settings.py

--- celery_settings.py
+++ celery_settings.py
@@ -3,7 +3,7 @@ from .celery import *
 broker_url = 'amqp://guest:guest@localhost:5672//'
 result_backend = 'redis://localhost:6379/0'
 if os.getenv('RABBIT_PORT') is not None and os.getenv('REDIS_PORT') is not None:
-    broker_url = 'amqp://guest:guest@rabbit:5672'
+    broker_url = 'amqp://guest:guest@rabbit:5672//'
     result_backend = 'redis://redis:6379/0'
     CELERY_ENABLED = True
     EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
